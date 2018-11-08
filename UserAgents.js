f67bee7efa6fa40738b020abe314ee059ffd54a4
HM The Mister Techno <giorgos4006@gmail.com>
2018-11-08 22:38:23 +0200

    Uploading JS Files before build upgrade

---
+++ UserAgents.js
@@ -0,0 +1,25 @@
+﻿/* Make SmartTVs recognize cursors */
+if (navigator.userAgent.match("SmartTV")) {
+document.getElementById("Handler").className += " smart"
+}
+
+if (navigator.userAgent.match("Macintosh")) {
+document.getElementById("Handler").className += " osx"
+}
+
+if (navigator.userAgent.match("Linux")) {
+document.getElementById("Handler").className += " xubuntu"
+}
+
+if (navigator.userAgent.match("Trident")) {
+document.getElementById("Handler").className += " explorer"
+}
+
+if (navigator.userAgent.match("Edge")) {
+document.getElementById("Handler").className += " edge"
+}
+
+/* Mobile Only CSS */
+if (navigator.userAgent.match("Mobile")) {
+document.getElementById("Handler").className += " phone"
+}
