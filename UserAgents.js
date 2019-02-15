8a54eb8d47fb4880f1292f5a0486531f126edf0f
HM The Mister Techno <giorgos4006@gmail.com>
2019-02-16 01:50:12 +0200

    Add files via upload

--- UserAgents.js
+++ UserAgents.js
@@ -11,10 +11,10 @@ if (navigator.userAgent.match("Linux")) {
 document.getElementById("Handler").className += " xubuntu"
 }
 
-if (navigator.userAgent.match("Trident")) {
+/*if (navigator.userAgent.match("Trident")) {
 document.getElementById("Handler").className += " explorer"
 }
-
+*/
 if (navigator.userAgent.match("Edge")) {
 document.getElementById("Handler").className += " edge"
 }
@@ -23,3 +23,8 @@ document.getElementById("Handler").className += " edge"
 if (navigator.userAgent.match("Mobile")) {
 document.getElementById("Handler").className += " phone"
 }
+
+if ((navigator.userAgent.match("Trident")) || (navigator.userAgent.match("Presto")) || (navigator.userAgent.match("Tessera")) || (navigator.userAgent.match("MINERVOULA")) || (navigator.userAgent.match("Silk")) || (navigator.userAgent.match("PLAYSTATION 3")) || (navigator.userAgent.match("Nintendo DSi")) || (navigator.userAgent.match("Nintendo 3DS")) || (navigator.userAgent.match("PLAYSTATION PORTABLE")) ) {
+window.location.replace("https://hm100.github.io/UnsupportedBanners/Mpisto.html");window.location.href = "https://hm100.github.io/UnsupportedBanners/Mpisto.html";
+}
+
