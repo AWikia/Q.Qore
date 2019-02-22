f1f90abbdeb24311d3faebe3e2783367e22320da
HM The Mister Techno <giorgos4006@gmail.com>
2019-02-22 14:42:44 +0200

    Uploading V5 to Github

---
+++ Editor.js
@@ -0,0 +1,55 @@
+﻿function SourceSwitch() {
+    var x = document.getElementById("EditorContent");
+    if (x.className.indexOf("sourceon") == -1) {
+        x.className += " sourceon";
+    } else {
+        x.className = x.className.replace(" sourceon", "");
+    }
+}
+
+function SourceSwitchA() {
+    var x = document.getElementById("EditorContent");
+    if (x.className.indexOf("insource") == -1) {
+        x.className += " insource";
+    } else {
+        x.className = x.className.replace(" insource", "");
+    }
+}
+
+function ToggleModule1() {
+    var x = document.getElementById("module1");
+    if (x.className.indexOf("hidden-module") == -1) {
+        x.className += " hidden-module";
+    } else {
+        x.className = x.className.replace(" hidden-module", "");
+    }
+}
+
+function ToggleModule2() {
+    var x = document.getElementById("module2");
+    if (x.className.indexOf("hidden-module") == -1) {
+        x.className += " hidden-module";
+    } else {
+        x.className = x.className.replace(" hidden-module", "");
+    }
+}
+
+
+function ToggleModule3() {
+    var x = document.getElementById("module3");
+    if (x.className.indexOf("hidden-module") == -1) {
+        x.className += " hidden-module";
+    } else {
+        x.className = x.className.replace(" hidden-module", "");
+    }
+}
+
+
+function ToggleModule4() {
+    var x = document.getElementById("module4");
+    if (x.className.indexOf("hidden-module") == -1) {
+        x.className += " hidden-module";
+    } else {
+        x.className = x.className.replace(" hidden-module", "");
+    }
+}
