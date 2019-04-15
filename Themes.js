5ab5432aba6c1f900043f9679f848e70545f83ce
HM The Mister Techno <giorgos4006@gmail.com>
2019-04-15 16:34:49 +0300

    Add files via upload

--- Themes.js
+++ Themes.js
@@ -1,7 +1,166 @@
 ﻿var isOptions = document.getElementById("Handler").className.indexOf("options") !== -1;
 (function () {
 document.querySelector('html').className += " theme-A"; // We begin with the first theme selected
+ColorUpdate();
 })();
+
+function UpdateValue() {
+var linkcolor1final = chroma($('input[type="color"][name="linkcolor"]').val()).get('rgb.r') + ',' + chroma($('input[type="color"][name="linkcolor"]').val()).get('rgb.g') + ',' + chroma($('input[type="color"][name="linkcolor"]').val()).get('rgb.b'); 
+var headercolorfinal = chroma($('input[type="color"][name="header"]').val()).get('rgb.r') + ',' + chroma($('input[type="color"][name="header"]').val()).get('rgb.g') + ',' + chroma($('input[type="color"][name="header"]').val()).get('rgb.b'); 
+
+	if ($("html.theme-A").length) {
+		$("style.designer-style").append(
+		'html.theme-A:not(.win10) {' +
+		'--background-color:' + $('input[type="color"][name="bg"]').val() + '!important;' +
+		'--link-color:' + linkcolor1final + '!important;' +
+		'--content-bg:' + $('input[type="color"][name="contentbg"]').val() + '!important;' +
+		'--content-border:' + $('input[type="color"][name="border"]').val() + '!important;' +
+		'--content-color:' + $('input[type="color"][name="contentcolor"]').val() + '!important;' +
+		'--button-color:' + $('input[type="color"][name="buttoncolor"]').val() + '!important;' +
+		'--community-header-bg:' + headercolorfinal + '!important;' +
+		'}'
+		);	
+	}
+
+	if ($("html.theme-B").length) {
+		$("style.designer-style").append(
+		'html.theme-B:not(.win10) {' +
+		'--background-color:' + $('input[type="color"][name="bg"]').val() + '!important;' +
+		'--link-color:' + linkcolor1final + '!important;' +
+		'--content-bg:' + $('input[type="color"][name="contentbg"]').val() + '!important;' +
+		'--content-border:' + $('input[type="color"][name="border"]').val() + '!important;' +
+		'--content-color:' + $('input[type="color"][name="contentcolor"]').val() + '!important;' +
+		'--button-color:' + $('input[type="color"][name="buttoncolor"]').val() + '!important;' +
+		'--community-header-bg:' + headercolorfinal + '!important;' +
+		'}'
+		);	
+	}
+
+	if ($("html.theme-C").length) {
+		$("style.designer-style").append(
+		'html.theme-C:not(.win10) {' +
+		'--background-color:' + $('input[type="color"][name="bg"]').val() + '!important;' +
+		'--link-color:' + linkcolor1final + '!important;' +
+		'--content-bg:' + $('input[type="color"][name="contentbg"]').val() + '!important;' +
+		'--content-border:' + $('input[type="color"][name="border"]').val() + '!important;' +
+		'--content-color:' + $('input[type="color"][name="contentcolor"]').val() + '!important;' +
+		'--button-color:' + $('input[type="color"][name="buttoncolor"]').val() + '!important;' +
+		'--community-header-bg:' + headercolorfinal + '!important;' +
+		'}'
+		);	
+	}
+
+	if ($("html.theme-D").length) {
+		$("style.designer-style").append(
+		'html.theme-D:not(.win10) {' +
+		'--background-color:' + $('input[type="color"][name="bg"]').val() + '!important;' +
+		'--link-color:' + linkcolor1final + '!important;' +
+		'--content-bg:' + $('input[type="color"][name="contentbg"]').val() + '!important;' +
+		'--content-border:' + $('input[type="color"][name="border"]').val() + '!important;' +
+		'--content-color:' + $('input[type="color"][name="contentcolor"]').val() + '!important;' +
+		'--button-color:' + $('input[type="color"][name="buttoncolor"]').val() + '!important;' +
+		'--community-header-bg:' + headercolorfinal + '!important;' +
+		'}'
+		);	
+	}
+
+	ColorUpdate();
+}
+
+function ColorUpdate() {
+/** Button Color **/
+/* Set Vars */
+var button_color = getComputedStyle(document.querySelector('html')).getPropertyValue("--button-color");
+
+if ((chroma(button_color).luminance()) > 0.3) {
+var buttoncolor1 = chroma(button_color).brighten(-0.6);
+var buttoncolor2 = 'black';
+} else {
+var buttoncolor1 = chroma(button_color).brighten(0.6);
+var buttoncolor2 = 'white';
+
+}
+
+/* Set Values */
+document.querySelector('body').style.setProperty("--button-color-dark", buttoncolor1);
+document.querySelector('body').style.setProperty("--button-color-text", buttoncolor2);
+$('input[type="color"][name="buttoncolor"]').val(chroma(button_color));
+
+
+/** Header Color **/
+/* Set Vars */
+var header_color =	'rgb(' + getComputedStyle(document.querySelector('html')).getPropertyValue("--community-header-bg") + ');';
+
+if ((chroma(header_color).luminance()) > 0.3) {
+var headercolor1 = '23,23,23';
+} else {
+var headercolor1 = '248,248,248';
+
+}
+
+/* Set Values */
+document.querySelector('body').style.setProperty("--community-header-text", headercolor1);
+$('input[type="color"][name="header"]').val(chroma(header_color));
+
+/** Link Color **/
+/* Set Vars */
+var link_color = 'rgb(' + getComputedStyle(document.querySelector('html')).getPropertyValue("--link-color") + ');';
+
+if ((chroma(link_color).luminance()) > 0.3) {
+var linkcolor1 = chroma(link_color).brighten(-0.6);
+var linkcolor2 = 'black';
+} else {
+var linkcolor1 = chroma(link_color).brighten(0.6);
+var linkcolor2 = 'white';
+
+}
+
+var linkcolor1final = chroma(linkcolor1).get('rgb.r') + ',' + chroma(linkcolor1).get('rgb.g') + ',' + chroma(linkcolor1).get('rgb.b'); 
+
+/* Set Values */
+document.querySelector('body').style.setProperty("--link-color-dark", linkcolor1final);
+document.querySelector('body').style.setProperty("--link-color-text", linkcolor2);
+$('input[type="color"][name="linkcolor"]').val(chroma(link_color));
+
+/** Content Border **/
+/* Set Vars */
+var border_color =	getComputedStyle(document.querySelector('html')).getPropertyValue("--content-border");
+
+if ((chroma(border_color).luminance()) > 0.3) {
+var bordercolor1 = chroma(border_color).brighten(-0.6);
+} else {
+var bordercolor1 = chroma(border_color).brighten(0.6);
+}
+
+/* Set Values */
+document.querySelector('body').style.setProperty("--content-border-dark", bordercolor1);
+$('input[type="color"][name="border"]').val(chroma(border_color));
+
+/** Dropdown BG **/
+/* Set Vars */
+var content_color =	getComputedStyle(document.querySelector('html')).getPropertyValue("--content-bg");
+var content_text =	getComputedStyle(document.querySelector('html')).getPropertyValue("--content-color");
+var body_bg =	getComputedStyle(document.querySelector('html')).getPropertyValue("--background-color");
+
+if ((chroma(content_color).luminance()) > 0.8) {
+	var dropdowncolor = 'white';
+} else if ((chroma(content_color).luminance()) > 0.3) {
+var dropdowncolor = chroma.mix(content_color, 'black', 0.1);
+} else {
+var dropdowncolor = chroma.mix(content_color, 'white', 0.1);
+}
+
+
+var emphasiscolor = chroma.mix(content_color, link_color, 0.6);
+
+
+document.querySelector('body').style.setProperty("--dropdown-bg", dropdowncolor);
+document.querySelector('body').style.setProperty("--emphasis-bg", emphasiscolor);
+$('input[type="color"][name="bg"]').val(chroma(body_bg));
+$('input[type="color"][name="contentbg"]').val(chroma(content_color));
+$('input[type="color"][name="contentcolor"]').val(chroma(content_text));
+
+}
 
 function HCa() {
     var x = document.querySelector('html');
@@ -19,6 +178,7 @@ function HCa() {
 		document.getElementById("304").checked = false;
 		document.getElementById("305").checked = false;
 		}
+		ColorUpdate();
 }
 
 function HCb() {
@@ -37,6 +197,7 @@ function HCb() {
 		document.getElementById("304").checked = false;
 		document.getElementById("305").checked = false;
 		}
+		ColorUpdate();
 }
 
 function HCc() {
@@ -55,6 +216,7 @@ function HCc() {
 		document.getElementById("303").checked = false;
 		document.getElementById("305").checked = false;
 	}
+		ColorUpdate();
 }
 
 function HCd() {
@@ -73,6 +235,7 @@ function HCd() {
 		document.getElementById("303").checked = false;
 		document.getElementById("304").checked = false;
 		}
+		ColorUpdate();
 }
 
 function HCclear() {
@@ -82,6 +245,7 @@ function HCclear() {
        
        
         x.className = x.className.replace(" win10", "");
+		ColorUpdate();
 }
 
 function HCcustom0() {
@@ -93,6 +257,7 @@ function HCcustom0() {
         x.className += " win10";
     }
         x.className = x.className.replace(" basic", "");
+		ColorUpdate();
 }
 
 function HCcustom() {
@@ -102,6 +267,7 @@ function HCcustom() {
     }
         x.className = x.className.replace(" win10", "");
         x.className = x.className.replace(" basic", "");
+		ColorUpdate();
 }
 
 function HCcustom2() {
@@ -112,4 +278,5 @@ function HCcustom2() {
         x.className = x.className.replace(" contrast", "");
         x.className = x.className.replace(" win10", "");
 
+		ColorUpdate();
 }
