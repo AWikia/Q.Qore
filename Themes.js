3a361be86562587af78035d46368f6f8348af92b
HM The Mister Techno <giorgos4006@gmail.com>
2019-04-23 23:27:22 +0300

    Add files via upload

--- Themes.js
+++ Themes.js
@@ -345,13 +345,13 @@ function ColorUpdate() {
 /* Set Vars */
 var button_color = getComputedStyle(document.querySelector('html')).getPropertyValue("--button-color");
 
-if ((chroma(button_color).get('lab.l')) > 49) {
-var buttoncolor1 = chroma(button_color).brighten(-0.6);
+if ((chroma(button_color).get('lab.l')) > 54) {
+var buttoncolor1 = chroma.mix(button_color, 'black', 0.15);
 var buttoncolor2 = 'black';
 document.querySelector('body').style.setProperty("--button-color-blend-light", button_color);
 document.querySelector('body').style.setProperty("--button-color-blend", buttoncolor1);
 } else {
-var buttoncolor1 = chroma(button_color).brighten(0.6);
+var buttoncolor1 = chroma.mix(button_color, 'white', 0.15);
 var buttoncolor2 = 'white';
 document.querySelector('body').style.setProperty("--button-color-blend-light", buttoncolor1);
 document.querySelector('body').style.setProperty("--button-color-blend", button_color);
@@ -367,14 +367,14 @@ $('input[type="color"][name="buttoncolor"]').val(chroma(button_color));
 /* Set Vars */
 var header_color =	'rgb(' + getComputedStyle(document.querySelector('html')).getPropertyValue("--community-header-bg") + ');';
 
-if ((chroma(header_color).get('lab.l')) > 49) {
-var headercolor1 = chroma(header_color).brighten(-0.6);
+if ((chroma(header_color).get('lab.l')) > 54) {
+var headercolor1 = chroma.mix(header_color, 'black', 0.15);
 var headercolor2 = '0,0,0';
 var headercolor1final = chroma(headercolor1).get('rgb.r') + ',' + chroma(headercolor1).get('rgb.g') + ',' + chroma(headercolor1).get('rgb.b'); 
 document.querySelector('body').style.setProperty("--community-header-bg-blend-light", header_color);
 document.querySelector('body').style.setProperty("--community-header-bg-blend", headercolor1final);
 } else {
-var headercolor1 = chroma(header_color).brighten(0.6);
+var headercolor1 = chroma.mix(header_color, 'white', 0.15);
 var headercolor2 = '255,255,255';
 var headercolor1final = chroma(headercolor1).get('rgb.r') + ',' + chroma(headercolor1).get('rgb.g') + ',' + chroma(headercolor1).get('rgb.b'); 
 document.querySelector('body').style.setProperty("--community-header-bg-blend-light", headercolor1final);
@@ -390,14 +390,14 @@ $('input[type="color"][name="header"]').val(chroma(header_color));
 /* Set Vars */
 var link_color = 'rgb(' + getComputedStyle(document.querySelector('html')).getPropertyValue("--link-color") + ');';
 
-if ((chroma(link_color).get('lab.l')) > 49) {
-var linkcolor1 = chroma(link_color).brighten(-0.6);
+if ((chroma(link_color).get('lab.l')) > 54) {
+var linkcolor1 = chroma.mix(link_color, 'black', 0.15);
 var linkcolor2 = 'black';
 var linkcolor1final = chroma(linkcolor1).get('rgb.r') + ',' + chroma(linkcolor1).get('rgb.g') + ',' + chroma(linkcolor1).get('rgb.b'); 
 document.querySelector('body').style.setProperty("--link-color-blend-light", link_color);
 document.querySelector('body').style.setProperty("--link-color-blend", linkcolor1final);
 } else {
-var linkcolor1 = chroma(link_color).brighten(0.6);
+var linkcolor1 = chroma.mix(link_color, 'white', 0.15);
 var linkcolor2 = 'white';
 var linkcolor1final = chroma(linkcolor1).get('rgb.r') + ',' + chroma(linkcolor1).get('rgb.g') + ',' + chroma(linkcolor1).get('rgb.b'); 
 document.querySelector('body').style.setProperty("--link-color-blend-light", linkcolor1final);
@@ -414,13 +414,13 @@ $('input[type="color"][name="linkcolor"]').val(chroma(link_color));
 /* Set Vars */
 var border_color =	getComputedStyle(document.querySelector('html')).getPropertyValue("--content-border");
 
-if ((chroma(border_color).get('lab.l')) > 49) {
-var bordercolor1 = chroma(border_color).brighten(-0.6);
+if ((chroma(border_color).get('lab.l')) > 54) {
+var bordercolor1 = chroma.mix(border_color, 'black', 0.15);
 var bordercolor2 = 'black';
 document.querySelector('body').style.setProperty("--content-border-blend-light", border_color);
 document.querySelector('body').style.setProperty("--content-border-blend", bordercolor1);
 } else {
-var bordercolor1 = chroma(border_color).brighten(0.6);
+var bordercolor1 = chroma.mix(border_color, 'white', 0.15);
 var bordercolor2 = 'white';
 document.querySelector('body').style.setProperty("--content-border-blend-light", bordercolor1);
 document.querySelector('body').style.setProperty("--content-border-blend", border_color);
@@ -439,18 +439,19 @@ var body_bg =	getComputedStyle(document.querySelector('html')).getPropertyValue(
 
 if ((chroma(content_color).get('lab.l')) > 89) {
 	var dropdowncolor = 'white';
-} else if ((chroma(content_color).get('lab.l')) > 49) {
-var dropdowncolor = chroma.mix(content_color, 'black', 0.1);
+} else if ((chroma(content_color).get('lab.l')) > 54) {
+var dropdowncolor = chroma.mix(content_color, 'black', 0.15);
 } else {
-var dropdowncolor = chroma.mix(content_color, 'white', 0.1);
+var dropdowncolor = chroma.mix(content_color, 'white', 0.15);
 }
 
 
-var emphasiscolor = chroma.mix(content_color, link_color, 0.6);
-
+var emphasiscolor = chroma.mix(content_color, link_color, 0.55);
+var emphasiscolor2 = chroma.mix(border_color, button_color, 0.55);
 
 document.querySelector('body').style.setProperty("--dropdown-bg", dropdowncolor);
 document.querySelector('body').style.setProperty("--emphasis-bg", emphasiscolor);
+document.querySelector('body').style.setProperty("--accent-bg", emphasiscolor2);
 $('input[type="color"][name="bg"]').val(chroma(body_bg));
 $('input[type="color"][name="contentbg"]').val(chroma(content_color));
 $('input[type="color"][name="contentcolor"]').val(chroma(content_text));
