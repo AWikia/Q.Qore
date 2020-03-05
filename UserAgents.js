35c98268d4495a74268b7caf42fe665ae43b5149
HM The Mister Techno <giorgos4006@gmail.com>
2020-03-06 01:56:05 +0200

    Release Build 101

--- UserAgents.js
+++ UserAgents.js
@@ -15,9 +15,9 @@ document.getElementById("Handler").className += " xubuntu"
 document.getElementById("Handler").className += " explorer"
 }
 */
-if (navigator.userAgent.match("Edge")) {
-document.getElementById("Handler").className += " edge"
-}
+//if (navigator.userAgent.match("Edge")) {
+//document.getElementById("Handler").className += " edge"
+//}
 
 /* Mobile Only CSS */
 if (navigator.userAgent.match("Mobile")) {
@@ -28,6 +28,11 @@ if ((navigator.userAgent.match("Trident")) || (navigator.userAgent.match("Presto
 window.location.replace("https://hm100.github.io/UnsupportedBanners/Mpisto.html");window.location.href = "https://hm100.github.io/UnsupportedBanners/Mpisto.html";
 }
 
+// Some old Fox Versions
+window.oldfox = ((navigator.userAgent.match("Firefox/31\\.")) || (navigator.userAgent.match("Firefox/32\\.")) || (navigator.userAgent.match("Firefox/33\\.")) || (navigator.userAgent.match("Firefox/34\\.")) || (navigator.userAgent.match("Firefox/35\\.")) || (navigator.userAgent.match("Firefox/36\\.")) || (navigator.userAgent.match("Firefox/37\\.")) || (navigator.userAgent.match("Firefox/38\\.")) || (navigator.userAgent.match("Firefox/39\\.")) || (navigator.userAgent.match("Firefox/40\\.")) || (navigator.userAgent.match("Firefox/41\\.")) || (navigator.userAgent.match("Firefox/42\\.")) || (navigator.userAgent.match("Firefox/43\\.")) || (navigator.userAgent.match("Firefox/44\\.")) || (navigator.userAgent.match("Firefox/45\\.")) || (navigator.userAgent.match("Firefox/46\\.")) || (navigator.userAgent.match("Firefox/47\\.")) || (navigator.userAgent.match("Firefox/48\\.")) || (navigator.userAgent.match("Firefox/49\\.")) || (navigator.userAgent.match("Firefox/50\\.")) || (navigator.userAgent.match("Firefox/51\\.")) || (navigator.userAgent.match("Firefox/52\\.")) || (navigator.userAgent.match("Firefox/53\\.")) || (navigator.userAgent.match("Firefox/54\\.")) || (navigator.userAgent.match("Firefox/55\\.")) || (navigator.userAgent.match("Firefox/56\\.")) || (navigator.userAgent.match("Firefox/21\\.")) || (navigator.userAgent.match("Firefox/20\\.")) || (navigator.userAgent.match("Firefox/19\\.")) || (navigator.userAgent.match("Firefox/18\\.")) || (navigator.userAgent.match("Firefox/17\\.")) || (navigator.userAgent.match("Firefox/16\\.")) || (navigator.userAgent.match("Firefox/15\\.")) || (navigator.userAgent.match("Firefox/14\\.")) || (navigator.userAgent.match("Firefox/13\\.")) || (navigator.userAgent.match("Firefox/12\\.")) || (navigator.userAgent.match("Firefox/11\\.")) || (navigator.userAgent.match("Firefox/10\\.")) || (navigator.userAgent.match("Firefox/9\\.")) || (navigator.userAgent.match("Firefox/8\\.")) || (navigator.userAgent.match("Firefox/7\\.")) || (navigator.userAgent.match("Firefox/6\\.")) || (navigator.userAgent.match("Firefox/5\\.")) || (navigator.userAgent.match("Firefox/4\\.")) || (navigator.userAgent.match("Firefox/3\\.")) || (navigator.userAgent.match("Firefox/2\\.")) || (navigator.userAgent.match("Firefox/1\\.")) )
+
+// Some old Google Versions
+window.oldchrome =  ((navigator.userAgent.match("Chrome/48\\.")) || (navigator.userAgent.match("Chrome/49\\.")) || (navigator.userAgent.match("Chrome/50\\.")) || (navigator.userAgent.match("Chrome/51\\.")) || (navigator.userAgent.match("Chrome/52\\.")) || (navigator.userAgent.match("Chrome/53\\.")) || (navigator.userAgent.match("Chrome/54\\.")) || (navigator.userAgent.match("Chrome/55\\.")) || (navigator.userAgent.match("Chrome/56\\.")) || (navigator.userAgent.match("Chrome/27\\.")) || (navigator.userAgent.match("Chrome/26\\.")) || (navigator.userAgent.match("Chrome/25\\.")) || (navigator.userAgent.match("Chrome/24\\.")) || (navigator.userAgent.match("Chrome/23\\.")) || (navigator.userAgent.match("Chrome/22\\.")) || (navigator.userAgent.match("Chrome/21\\.")) || (navigator.userAgent.match("Chrome/20\\.")) || (navigator.userAgent.match("Chrome/19\\.")) || (navigator.userAgent.match("Chrome/18\\.")) || (navigator.userAgent.match("Chrome/17\\.")) || (navigator.userAgent.match("Chrome/16\\.")) || (navigator.userAgent.match("Chrome/15\\.")) || (navigator.userAgent.match("Chrome/14\\.")) || (navigator.userAgent.match("Chrome/13\\.")) || (navigator.userAgent.match("Chrome/12\\.")) || (navigator.userAgent.match("Chrome/11\\.")) || (navigator.userAgent.match("Chrome/10\\.")) || (navigator.userAgent.match("Chrome/9\\.")) || (navigator.userAgent.match("Chrome/8\\.")) || (navigator.userAgent.match("Chrome/7\\.")) || (navigator.userAgent.match("Chrome/6\\.")) || (navigator.userAgent.match("Chrome/5\\.")) || (navigator.userAgent.match("Chrome/4\\.")) || (navigator.userAgent.match("Chrome/3\\.")) || (navigator.userAgent.match("Chrome/2\\.")) || (navigator.userAgent.match("Chrome/1\\.")) )
 
 //$(' .wds-dropdown__toggle').attr('onclick', 'DropdownSwitch()')
 
@@ -74,4 +79,58 @@ window.MW18newnavblock=false;
 	}
 })();
 
+if ( (navigator.userAgent.match("Edge")) || (navigator.userAgent.match("BlackBerry")) || (navigator.userAgent.match("IEMobile")) || window.oldchrome || window.oldfox || !(CSS.supports("--s","none"))  ) {
+$('.mpisto-content .mpisto-wrapper').prepend ( 
+      '<div class="wds-banner-notification__container browser-warning" id="floatingbanner2">' +
+        '<div class="wds-banner-notification wds-warning" id="BannerBrowser">' +
+          '<div class="wds-banner-notification__icon">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" class="wds-icon wds-icon-small">' +
+              '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#wds-icons-alert-small" />' +
+            '</svg>' +
+          '</div>' +
+          '<span class="wds-banner-notification__text"> We\'re dropping support on your browser soon. Please make sure that you must update your browser before its too late!<br>If your device doesn\'t allow you to upgrade your browser or can\'t afford a cutting-edge browser, this means we\'re also dropping support on your device.<br>You can download <a href="https://www.google.com/chrome/">Google Chrome</a>, <a href="https://www.mozilla.org/firefox/">Mozilla Firefox</a> or the <a href="https://support.microsoft.com/help/4501095/download-the-new-microsoft-edge-based-on-chromium">Chromium Edge</a> if you want to still use Qora Qore.</span>' +
+          '<svg onclick="RemoveBannerBrowser()" xmlns="http://www.w3.org/2000/svg" class="wds-banner-notification__close wds-icon wds-icon-tiny">' +
+            '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#wds-icons-cross-tiny" />' +
+          '</svg>' +
+        '</div>' +
+      '</div>'
+
+);
+
+$('.mpisto-options-content').prepend ( 
+      '<div class="wds-banner-notification__container browser-warning" id="floatingbanner2">' +
+        '<div class="wds-banner-notification wds-warning" id="BannerBrowser">' +
+          '<div class="wds-banner-notification__icon">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" class="wds-icon wds-icon-small">' +
+              '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#wds-icons-alert-small" />' +
+            '</svg>' +
+          '</div>' +
+          '<span class="wds-banner-notification__text"> We\'re dropping support on your browser soon. Please make sure that you must update your browser before its too late!<br>If your device doesn\'t allow you to upgrade your browser or can\'t afford a cutting-edge browser, this means we\'re also dropping support on your device.<br>You can download <a href="https://www.google.com/chrome/">Google Chrome</a>, <a href="https://www.mozilla.org/firefox/">Mozilla Firefox</a> or the <a href="https://support.microsoft.com/help/4501095/download-the-new-microsoft-edge-based-on-chromium">Chromium Edge</a> if you want to still use Qora Qore.</span>' +
+          '<svg onclick="RemoveBannerBrowser()" xmlns="http://www.w3.org/2000/svg" class="wds-banner-notification__close wds-icon wds-icon-tiny">' +
+            '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#wds-icons-cross-tiny" />' +
+          '</svg>' +
+        '</div>' +
+      '</div>'
+
+);
+
+
+}
+
+function RemoveBannerBrowser() {
+    var x = document.getElementById("BannerBrowser");
+        x.className += " wds-is-transparent";
+	    setTimeout(RemoveBannerBrowser1, 405) 
+}
+
+function RemoveBannerBrowser1() {
+    var x = document.getElementById("BannerBrowser");
+	document.getElementById("floatingbanner2").removeChild(x);
+	if ($(".mpisto-content").length) {
+		document.querySelector(".mpisto-content .mpisto-wrapper").removeChild(document.getElementById("floatingbanner2"));
+	}
+	if ($(".mpisto-options-content").length) {
+		document.querySelector(".mpisto-options-content").removeChild(document.getElementById("floatingbanner2"));
+	}
+}
 
